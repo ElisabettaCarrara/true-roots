46405a732e16eb6db9d5b752a1d9d34f0a585899
ELI.CA <elisabetta.marina.clelia@gmail.com>
2022-10-19 17:58:19 +0200

    aggioranti i file
    creato un sistema di shopping cart con pagina carrello (la pagina di menu dove si possono aggiungere i prodotti funziona, la pagina carrello non riporta i prodotti. devo aggiungere al carrello il form di input dei dati (indirizzo etc)

---
+++ true_roots/assets/js/Data.js
@@ -0,0 +1,223 @@
+let shopItemsData = [
+  {
+    id: "marinara",
+    name: "Marinara",
+    price: 6.80,
+    desc: "Passata di pomodoro, olio evo, aglio, origano",
+  },
+  {
+    id: "napoletana",
+    name: "Napoletana",
+    price: 7.80,
+    desc: "Passata di pomodoro, mozzarella fior di latte, acciughe, capperi, origano e olio evo",
+  },
+  {
+    id: "margherita",
+    name: "Margherita",
+    price: 7,
+    desc: "Passata di pomodoro, mozzarella fior di latte, basilico e olio evo",
+  },
+  {
+    id: "misticanza",
+    name: "misticanza",
+    price: 9.20,
+    desc: "Mozzarella fior di latte, insalata misticanza, pomodorini, julienne di zucchine e carote",
+  },
+  {
+    id: "vegetariana",
+    name: "Vegetariana",
+    price: 10,
+    desc: "Passata di pomodoro, mozzarella fior di latte, verdure grigliate, pomodorini confit, basilico e olio evo",
+  },
+  {
+    id: "stagionata",
+    name: "Stagionata",
+    price: 12,
+    desc: "Passata di pomodoro, mozzarella fior di latte, bacon croccante, ricotta stagionata e zest di lime",
+  },
+  {
+    id: "bolognese",
+    name: "Bolognese",
+    price: 12,
+    desc: "Mozzarella fior di latte, crema di burrata pugliese, mortadella di Bologna IGP e pistacchi.",
+  },
+  {
+    id: "parma",
+    name: "Parma",
+    price: 12.50,
+    desc: "Passata di pomodoro, mozzarella fior di latte, mozzarella di bufala campana DOP, prosciutto crudo di Parma e rucola",
+  },
+  {
+    id: "solemare",
+    name: "Sole e Mare",
+    price: 13,
+    desc: "Passata di pomodoro, mozzarella di bufala campana DOP, acciughe del Mar Cantabrico, sfilacci di pomodoro secco e olive taggiasche",
+  },
+  {
+    id: "norma",
+    name: "Norma 2.0",
+    price: 13.80,
+    desc: "Mozzarella fior di latte, mozzarella di bufala campana DOP, crema di melanzane fumé, melanzane al forno, pomodorini confit, pinoli tostati e guanciale amatriciano croccante",
+  },
+  {
+    id: "quattroformaggi",
+    name: "4 Formaggi",
+    price: 10,
+    desc: "Mozzarella fior di latte, crema di gorgonzola DOP, provola fumé e pecorino",
+  },
+  {
+    id: "twister",
+    name: "Twister",
+    price: 10,
+    desc: "Passata di pomodoro, crema di stracciatella, pomodorini confit, crema di basilico e olio evo",
+  },
+  {
+    id: "contadina",
+    name: "Contadina",
+    price: 15.50,
+    desc: "Mozzarella fior di latte, cubetti di melanzane, bacon croccante, crema gorgonzola e cipolle bionde del pastore",
+  },
+  {
+    id: "venice",
+    name: "Venice",
+    price: 11,
+    desc: "Passata di pomodoro, mozzarella fior di latte, prosciutto cotto Gran Biscotto porcini e funghi misti",
+  },
+  {
+    id: "friarelli",
+    name: "Friarelli",
+    price: 11.80,
+    desc: "Passata di pomodoro, mozzarella fior di latte, friarelli, salsiccia fresca pecorino e olio evo.",
+  },
+  {
+    id: "piccantina",
+    name: "Piccantina",
+    price: 11.80,
+    desc: "Passata di pomodoro, mozzarella fior di latte, mozzarella di bufala campana DOP, salame piccante, rucola e pomodorini",
+  },
+  {
+    id: "norvegese",
+    name: "Norvegese",
+    price: 13.80,
+    desc: "Mozzarella fior di latte, salmone marinato, zucchine alla citronette, pomodorini confit, mandorle tostate e rucola",
+  },
+  {
+    id: "spadellata",
+    name: "Spaellata di Mare",
+    price: 14,
+    desc: "passata di pomodoro, mozzarella fior di latte, spadellata di totani, calamare e gamberetti",
+  },
+  
+  
+
+
+  
+  {
+    id: "marinara",
+    name: "Marinara",
+    price: 6.80,
+    desc: "Passata di pomodoro, olio evo, aglio, origano",
+  },
+  {
+    id: "napoletana",
+    name: "Napoletana",
+    price: 7.80,
+    desc: "Passata di pomodoro, mozzarella fior di latte, acciughe, capperi, origano e olio evo",
+  },
+  {
+    id: "margherita",
+    name: "Margherita",
+    price: 7,
+    desc: "Passata di pomodoro, mozzarella fior di latte, basilico e olio evo",
+  },
+  {
+    id: "misticanza",
+    name: "misticanza",
+    price: 9.20,
+    desc: "Mozzarella fior di latte, insalata misticanza, pomodorini, julienne di zucchine e carote",
+  },
+  {
+    id: "vegetariana",
+    name: "Vegetariana",
+    price: 10,
+    desc: "Passata di pomodoro, mozzarella fior di latte, verdure grigliate, pomodorini confit, basilico e olio evo",
+  },
+  {
+    id: "stagionata",
+    name: "Stagionata",
+    price: 12,
+    desc: "Passata di pomodoro, mozzarella fior di latte, bacon croccante, ricotta stagionata e zest di lime",
+  },
+  {
+    id: "bolognese",
+    name: "Bolognese",
+    price: 12,
+    desc: "Mozzarella fior di latte, crema di burrata pugliese, mortadella di Bologna IGP e pistacchi.",
+  },
+  {
+    id: "parma",
+    name: "Parma",
+    price: 12.50,
+    desc: "Passata di pomodoro, mozzarella fior di latte, mozzarella di bufala campana DOP, prosciutto crudo di Parma e rucola",
+  },
+  {
+    id: "solemare",
+    name: "Sole e Mare",
+    price: 13,
+    desc: "Passata di pomodoro, mozzarella di bufala campana DOP, acciughe del Mar Cantabrico, sfilacci di pomodoro secco e olive taggiasche",
+  },
+  {
+    id: "norma",
+    name: "Norma 2.0",
+    price: 13.80,
+    desc: "Mozzarella fior di latte, mozzarella di bufala campana DOP, crema di melanzane fumé, melanzane al forno, pomodorini confit, pinoli tostati e guanciale amatriciano croccante",
+  },
+  {
+    id: "quattroformaggi",
+    name: "4 Formaggi",
+    price: 10,
+    desc: "Mozzarella fior di latte, crema di gorgonzola DOP, provola fumé e pecorino",
+  },
+  {
+    id: "twister",
+    name: "Twister",
+    price: 10,
+    desc: "Passata di pomodoro, crema di stracciatella, pomodorini confit, crema di basilico e olio evo",
+  },
+  {
+    id: "contadina",
+    name: "Contadina",
+    price: 15.50,
+    desc: "Mozzarella fior di latte, cubetti di melanzane, bacon croccante, crema gorgonzola e cipolle bionde del pastore",
+  },
+  {
+    id: "venice",
+    name: "Venice",
+    price: 11,
+    desc: "Passata di pomodoro, mozzarella fior di latte, prosciutto cotto Gran Biscotto porcini e funghi misti",
+  },
+  {
+    id: "friarelli",
+    name: "Friarelli",
+    price: 11.80,
+    desc: "Passata di pomodoro, mozzarella fior di latte, friarelli, salsiccia fresca pecorino e olio evo.",
+  },
+  {
+    id: "piccantina",
+    name: "Piccantina",
+    price: 11.80,
+    desc: "Passata di pomodoro, mozzarella fior di latte, mozzarella di bufala campana DOP, salame piccante, rucola e pomodorini",
+  },
+  {
+    id: "norvegese",
+    name: "Norvegese",
+    price: 13.80,
+    desc: "Mozzarella fior di latte, salmone marinato, zucchine alla citronette, pomodorini confit, mandorle tostate e rucola",
+  },
+  {
+    id: "spadellata",
+    name: "Spaellata di Mare",
+    price: 14,
+    desc: "passata di pomodoro, mozzarella fior di latte, spadellata di totani, calamare e gamberetti",
+  },
+];
